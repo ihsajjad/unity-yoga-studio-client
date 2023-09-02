315702ce79f87859c9ca4a00b8b8668a2932b122
ihsajjad <ihsajjad1@gmail.com>
2023-09-02 16:09:34 +0600

    added content to the tailwind.config file

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { router } from "./routes/routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <div className="max-w-7xl mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   </React.StrictMode>
 );
