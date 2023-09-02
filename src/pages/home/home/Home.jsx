3e1eb3a9fd6e37732a563e4d623d5effc57ae390
jumma <aarifhingorja4@gmail.com>
2023-09-02 16:53:35 +0530

    Completed the about page

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,5 +1,19 @@
+import About from "./components/About";
+import CallToAction from "./components/CallToAction";
+import NewsSection from "./components/NewsSection";
+
 const Home = () => {
-  return <div>This is home page</div>;
+  return (
+    <>
+      {/* About us  */}
+      <About />
+      {/* Call to action */}
+      <CallToAction />
+      {/* NewusLetter */}
+      <NewsSection />
+      
+    </>
+  )
 };
 
 export default Home;
