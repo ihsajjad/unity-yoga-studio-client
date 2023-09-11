650a63cc4aec0d78dabfc9b661bf34a4378655d2
jumma <aarifhingorja4@gmail.com>
2023-09-11 12:43:14 +0530

    improve newsSection in light mode

--- src/pages/home/NewsSection.jsx
+++ src/pages/home/NewsSection.jsx
@@ -1,29 +1,13 @@
+import newsSection from "../../assets/newsSection.png"
+
 const NewsSection = () => {
   return (
     // bg-fixed bg-center bg-cover bg-[url('https://images.unsplash.com/photo-1571103189706-cd54b23e81f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80')]
-    <section className="section-container w-auto h-full text-center bg-opacity-50  ">
-      {/* <div className="">
-     <h1 className="section-title">Subscribe To Our Newsletter</h1>
-      <p className="about-para">
-        {" "}
-        for our newsletter to get the latest news and updates about our upcoming
-        events and classes{" "}
-      </p>
-      <div className="flex items-center justify-center mt-5 flex-col gap-y-4 md:flex-row">
-        <input
-          type="text"
-          placeholder="Email Address"
-          className="py-2 px-3 rounded-lg outline-none border-2 border-[var(--main-color)] md:w-full max-w-xs "
-        /> 
-        <button className="@apply rounded-md  border-2 border-[var(--main-color)] md:py-2 py-1 md:px-3 px-2 bg-[var(--main-color)] text-white font-semibold duration-300 md:rounded-r-lg md:-ml-8">
-          Subscribe{" "}
-        </button>
-      </div>
-     </div> */}
+    <section className="section-container w-auto h-full text-center bg-opacity-50 bg-[var(--secondary-color)]">
       <div className="flex flex-col md:flex-row ">
         <div className="md:flex-1">
           <img
-            src="https://zakratheme.com/blog/wp-content/uploads/2023/06/Zakra-Blog-Newsletter-scaled.webp"
+            src={newsSection}
             alt="Image"
             className="w-full h-full"
           />
@@ -31,7 +15,7 @@ const NewsSection = () => {
         <div className="flex flex-col ml-0 md:ml-20 items-center md:items-stretch md:mt-14 gap-y-4">
           <h2 className="section-title w-[20rem] md:ml-7">Subscribe To Our Newsletter</h2>
           <p className="about-para w-[20rem]  md:ml-0 md:w-[25rem]">
-            Stay up-to-date with the latest news & insights from our blog by
+            Stay up-to-date with the latest news & insights from our blog by Unity Yoga Studio
             joining to our newsletter!
           </p>
           <div className="flex flex-col md:flex-row w-[20rem] md:w-[25rem]  gap-y-3  md:mx-0 md:ml-0">
