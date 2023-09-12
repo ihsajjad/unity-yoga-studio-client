866214f60ec356bc8e1838070eec0def241c391c
ihsajjad <ihsajjad1@gmail.com>
2023-09-12 22:39:52 +0600

    added needed elements

--- src/pages/classes/SingleClass.jsx
+++ src/pages/classes/SingleClass.jsx
@@ -1,8 +1,11 @@
 import { Rating } from "@smastrom/react-rating";
 import { useEffect, useState } from "react";
+import img_bg from "../../assets/img-bg.svg";
+import instructor_img from "../../assets/image-removebg-preview.png";
 
 const SingleClass = () => {
   const [singleClass, setSingleClass] = useState({});
+  const [instructorData, setInstructorData] = useState({});
   const {
     _id,
     name,
@@ -17,12 +20,10 @@ const SingleClass = () => {
     max_students,
     // class_size,
     // class_type,
-    // instructor,
+    instructor,
     ratings,
     reviews,
   } = singleClass;
-
-  console.log(reviews);
 
   useEffect(() => {
     fetch("classes.json")
@@ -30,11 +31,32 @@ const SingleClass = () => {
       .then((data) => setSingleClass(data[0]));
   }, []);
 
+  useEffect(() => {
+    fetch("instructors.json")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && instructor?.email) {
+          const matched = data.find(
+            (item) => item?.email === instructor?.email
+          );
+          setInstructorData(matched);
+        }
+      });
+  }, [instructor]);
+
+  console.log(instructorData);
+
   return (
     <section className="section-container space-y-10">
-      <div className="flex lg:gap-10 md:gap-5 gap-2">
-        <div className="flex-1">
-          <h3 className="text-2xl font-bold text-[var(--main-color)] mb-3">
+      <div className={`flex gap-2 min-h-[80vh] relative rounded`}>
+        <img
+          src={img}
+          alt=""
+          className="w-full h-full object-cover rounded absolute"
+        />
+        <div className="bg-black h-full w-full rounded absolute bg-opacity-30"></div>
+        <div className="flex-1 rounded-l cutom-shape p-10 bg-opacity-10">
+          <h3 className="text-2xl font-bold text-[var(--secondary-color)] mb-3">
             {name}
           </h3>
           <p>{description}</p>
@@ -50,16 +72,30 @@ const SingleClass = () => {
             <span>{ratings}/5</span>
           </div>
         </div>
-        <div className="flex-1">
-          <img
-            src={img}
-            alt=""
-            className="w-full h-full object-cover rounded"
-          />
+        <div className="flex-1"></div>
+      </div>
+
+      {/* Instructor's data */}
+      <div>
+        <h3 className="text-2xl font-bold text-[var(--main-color)] mb-3">
+          Trainer
+        </h3>
+        <div>
+          <p>{instructorData.name}</p>
+          <p>{instructorData.phone}</p>
+          <p>{instructorData.email}</p>
+          <p>{instructorData.teaching_philosophy}</p>
+        </div>
+        <div className="relative">
+          <div className="message-bg w-56 h-40 "></div>
+          <img src={img_bg} alt="" />
+          <img src={instructor_img} alt="" className="absolute top-0 left-0" />
         </div>
       </div>
 
+      {/*feedback area */}
       <div>
+        <h3 className="text-2xl font-bold mb-3">Write a review:</h3>
         <div className="w-full flex gap-2 items-end">
           <textarea
             name=""
