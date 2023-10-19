2d480b5667ce2c994e049f7f7b1d841166ee15eb
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-19 21:30:41 +0600

    setting added

--- src/pages/dashboard/Settings/Settings.jsx
+++ src/pages/dashboard/Settings/Settings.jsx
@@ -98,8 +98,8 @@ const Settings = () => {
                         </label>
                         <textarea className="textarea textarea-bordered" placeholder="enter hero sections heading"></textarea>
                     </div>
-                    <div className="form-control  mt-6 w-1/5">
-                        <button className="items-center border-2 border-[var(--secondary-color)] md:py-2 py-1 md:px-3 px-2 rounded-full bg-[var(--secondary-color)] text-white hover:bg-transparent hover:text-[var(--secondary-color)] font-semibold duration-300 w-full self-center" type="submit">Add Sub-heading</button>
+                    <div className="form-control mt-6 w-1/5">
+                        <button className="items-end border-2 border-[var(--secondary-color)] md:py-2 py-1 md:px-3 px-2 rounded-full bg-[var(--secondary-color)] text-white hover:bg-transparent hover:text-[var(--secondary-color)] font-semibold duration-300 w-full justify-end" type="submit">Add Sub-heading</button>
                     </div>
                 </div>
             </form>
